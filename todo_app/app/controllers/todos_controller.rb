566549e92316ccb8e25880b16cc2d2e79f4a3c0d
roberto.jofre.godoy <roberto.jofre.godoy@gmail.com>
2018-07-26 14:58:59 -0700

    add NAVBAR

--- todo_app/app/controllers/todos_controller.rb
+++ todo_app/app/controllers/todos_controller.rb
@@ -40,6 +40,11 @@ class TodosController < ApplicationController
         redirect_to todos_path
     end
 
+    def list
+        @todos = Todo.all
+
+    end
+
     private
     
     def todo_params
